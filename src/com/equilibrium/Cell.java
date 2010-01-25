dd3cfabd616ad3bfe953276b4315d659bea618ae
dm0388 <dm0388@16275113-ae20-a987-b40f-27e6ba3916c3>
2010-01-25 19:44:25 +0000

    Primo commit progetto

---
+++ src/com/equilibrium/Cell.java
@@ -0,0 +1,200 @@
+package com.equilibrium;
+
+import java.util.Vector;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Color;
+
+public class Cell extends View implements OnClickListener {
+
+	private Equilibrium e;
+	private String sign;
+	private String number;
+	private int size;
+	private int row;
+	private int col;
+	private boolean numberSetted = false;
+	private int bkColor = Color.GRAY;
+	private int borderColor = Color.BLACK;
+	private int signColor = Color.RED;
+	private int numberColor = Color.YELLOW;
+	private boolean[] around;
+	
+	 /**
+     * Constructor.  This version is only needed if you will be instantiating
+     * the object manually (not from a layout XML file).
+     * @param context
+     */
+    public Cell(Context context, int r, int c) {
+        super(context);
+        e = (Equilibrium) getContext();
+        setOnClickListener(this);
+        sign = "";
+        number = "";
+        row = r;
+        col = c;
+        around = new boolean[e.lato+1];
+        for (int i = 0; i <= e.lato; i++) {
+        	around[i] = false;
+        }
+    }
+    
+    /**
+     * @see android.view.View#measure(int, int)
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(measureWidth(widthMeasureSpec),
+                measureHeight(heightMeasureSpec));
+    }
+
+    /**
+     * Determines the width of this view
+     * @param measureSpec A measureSpec packed into an int
+     * @return The width of the view, honoring constraints from measureSpec
+     */
+    private int measureWidth(int measureSpec) {
+        int result = 0;
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+
+        if (specMode == MeasureSpec.EXACTLY) {
+            // We were told how big to be
+            result = specSize;
+        } else {
+            // Measure the text
+            //result = (int) mTextPaint.measureText(mText) + getPaddingLeft()
+            //        + getPaddingRight();
+            if (specMode == MeasureSpec.AT_MOST) {
+                // Respect AT_MOST value if that was what is called for by measureSpec
+                result = Math.min(result, specSize);
+            }
+        }
+
+        size = result;
+        return result;
+    }
+
+    /**
+     * Determines the height of this view
+     * @param measureSpec A measureSpec packed into an int
+     * @return The height of the view, honoring constraints from measureSpec
+     */
+    private int measureHeight(int measureSpec) {
+    	return size;
+    }
+    
+    /**
+     * Render the text
+     * 
+     * @see android.view.View#onDraw(android.graphics.Canvas)
+     */
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        
+        canvas.drawColor(bkColor);
+        
+        Paint p = new Paint();
+        p.setAntiAlias(true);
+        p.setColor(borderColor);
+        canvas.drawLine(0, 0, getWidth(), 0, p);
+        canvas.drawLine(0, 0, 0, getHeight(), p);
+        canvas.drawLine(getWidth(), 0, getWidth(), getHeight(), p);
+        canvas.drawLine(getWidth(), getHeight(), 0, getHeight(), p);
+        
+        p.setTextSize(10);
+        p.setColor(signColor);
+        canvas.drawText(sign, 2, 10, p);
+        
+        p.setTextSize(12);
+        p.setColor(numberColor);
+        canvas.drawText(number, (getWidth()/2)-(p.measureText(number)/2), (getHeight()/2)+4, p);
+    }
+    
+    public void setBkColor(int color) {
+    	bkColor = color;
+    	invalidate();
+    }
+    
+    public void setSignColor(int color) {
+    	signColor = color;
+    	invalidate();
+    }
+    
+    public void setSign(String txt) {
+    	sign = txt;
+    	invalidate();
+    }
+    
+    public String getSign() {
+    	return sign;
+    }
+    
+    public void setNumberColor(int color) {
+    	numberColor = color;
+    	invalidate();
+    }
+    
+    public void setNumber(String txt) {
+    	number = txt;
+    	numberSetted = true;
+    	//Aggiorno il punteggio totale
+    	if ((row != e.lato+1) && (col != e.lato+1)) {
+    		e.updateSum(row, col);
+    		e.setAround(row, col, Integer.parseInt(number));
+    	}
+    	invalidate();
+    }
+    
+    public int getNumber() {
+    	if (number != "") {
+    		return Integer.parseInt(number);
+    	} else {
+    		return 0;
+    	}
+    }
+    
+    public void setAround(int number) {
+    	around[number] = true;
+    }
+    
+    public Vector<Integer> getAvailableAround() {
+    	Vector<Integer> a = new Vector<Integer>();
+    	for (int i = 1; i <= e.lato; i++) {
+    		if (around[i] == false) {
+    			a.add(i);
+    		}
+    	}
+    	return a;
+    }
+    
+	public void onClick(View v) {
+		e.selectedRow = row;
+		e.selectedCol = col;
+		if ((numberSetted == false) && ((row != e.lato+1) && (col != e.lato+1))) {
+			/*Dialog tmp = new Dialog(getContext());
+			GridView gridview = new GridView(getContext());
+			
+			int[] num = new int[e.lato];
+			for (int i = 0; i < e.lato; i++) {
+				//Controllo i lati
+				num[i] = i+1;
+			}
+			
+			gridview.setNumColumns(GridView.AUTO_FIT);
+		    gridview.setAdapter(new ButtonAdapter(getContext(), num, tmp, this));
+		    
+		    tmp.setTitle("Scegli un numero");
+		    tmp.setContentView(gridview);
+			tmp.show();*/
+			e.showNumbers(row, col);
+		}
+		bkColor = Color.CYAN;
+		invalidate();
+	}
+}
---
+++ src/com/equilibrium/Equilibrium.java
@@ -0,0 +1,122 @@
+package com.equilibrium;
+
+import java.util.Vector;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
+public class Equilibrium extends Activity implements OnClickListener {
+	
+	public int lato = 6;
+	public Cell[][] amatriciana;
+	public int selectedRow = 0;
+	public int selectedCol = 0;
+	public LinearLayout l;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.main);
+        
+        l = new LinearLayout(this);
+        l.setOrientation(LinearLayout.VERTICAL);
+        
+        TableLayout a = new TableLayout(this);
+        a.setStretchAllColumns(true);
+        
+        amatriciana = new Cell[lato+2][];
+        
+        for (int i = 0; i < lato+2; i++) {
+        	amatriciana[i] = new Cell[lato+2];
+        	TableRow r = new TableRow(this);
+        	for (int j = 0; j < lato+2; j++) {
+        		amatriciana[i][j] = new Cell(this, i, j);
+        		if ((j < lato+1) && (i < lato+1)) {
+	        		String sign;
+	        		if ((i+j) % 2 == 0) {
+	        			sign = "+";
+	        			amatriciana[i][j].setSignColor(Color.GREEN);
+	        		} else {
+	        			sign = "-";
+	        			amatriciana[i][j].setSignColor(Color.RED);
+	        		}
+	        		amatriciana[i][j].setSign(sign);
+        		} else {
+        			amatriciana[i][j].setBkColor(Color.TRANSPARENT);
+        			amatriciana[i][j].setNumberColor(Color.BLUE);
+        		}
+        		if ((j != 0) && (i != 0)) {
+        			r.addView(amatriciana[i][j]);
+        		}
+        	}
+        	a.addView(r);
+        }
+        
+        l.addView(a);
+        
+        setContentView(l);
+    }
+    
+    public void updateSum(int row, int col) {
+    	int sumX = 0;
+    	int sumY = 0;
+    	for (int i = 1; i < lato+1; i++) {
+    		if (amatriciana[row][i].getSign() == "-") {
+    			sumX -= amatriciana[row][i].getNumber();
+    		} else {
+    			sumX += amatriciana[row][i].getNumber();
+    		}
+    		
+    		if (amatriciana[i][col].getSign() == "-") {
+    			sumY -= amatriciana[i][col].getNumber();
+    		} else {
+    			sumY += amatriciana[i][col].getNumber();
+    		}
+    	}
+    	amatriciana[row][lato+1].setNumber(Integer.toString(sumX));
+    	amatriciana[lato+1][col].setNumber(Integer.toString(sumY));
+    }
+    
+    public void setAround(int row, int col, int number) {
+    	amatriciana[row-1][col-1].setAround(number);
+    	amatriciana[row-1][col].setAround(number);
+    	amatriciana[row-1][col+1].setAround(number);
+    	amatriciana[row][col-1].setAround(number);
+    	amatriciana[row][col+1].setAround(number);
+    	amatriciana[row+1][col-1].setAround(number);
+    	amatriciana[row+1][col+1].setAround(number);
+    	amatriciana[row+1][col].setAround(number);
+    }
+    
+    public void showNumbers(int row, int col) {
+    	TableLayout b = new TableLayout(this);
+    	TableRow c = new TableRow(this);
+    	Vector<Integer> around = amatriciana[row][col].getAvailableAround();
+    	Button btn;
+    	for (int i = 0; i < around.size(); i++) {
+    		btn = new Button(this);
+            btn.setText(Integer.toString(around.elementAt(i)));
+            btn.setOnClickListener(this);
+            c.addView(btn);
+    	}
+    	b.addView(c);
+    	if (l.getChildCount() > 1) {
+    		l.removeViewAt(1);
+    	}
+    	l.addView(b, 1);
+    }
+    
+    public void onClick(View v) {
+		Button b = (Button) v;
+		amatriciana[selectedRow][selectedCol].setNumber((String) b.getText());
+		showNumbers(selectedRow, selectedCol);
+	}
+}
